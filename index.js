857356491459fa85189632c823afc48069c96f9b
fenota-viktoriya <Fen9206@gmail.com>
2021-12-18 00:28:27 +0200

    решает задачу ментора

---
+++ index.js
@@ -0,0 +1,18 @@
+let input;
+let result = 0;
+
+while (input !== null) {
+    input = prompt("Введите число");
+
+    if (input !== null) {
+        let numberInput=(Number(input));
+        
+        if (Number.isNaN(numberInput)) {
+            alert("Было введено не число, попробуйте еще раз");
+        } else {
+            result += numberInput;
+        }
+    }
+}
+
+alert(`Общая сумма чисел равна ${result}`);
